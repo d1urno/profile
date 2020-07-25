34395f6ef9251ea0905d74589bd154e105fdfdb0
Pablo Miceli <d1urno@gmx.com>
2020-07-25 13:19:11 -0300

    Initial project configurations

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier', 'only-warn'],
+  // add your custom rules here
+  rules: {
+    'max-len': ['warn', { code: 120 }],
+    'comma-dangle': ['warn', 'never']
+  }
+}
