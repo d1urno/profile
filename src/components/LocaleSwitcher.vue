566a00ef9ccb08794979e09c8a325b794e962e40
Pablo Miceli <d1urno@gmx.com>
2020-07-27 22:24:25 -0300

    Set cursor pointer on language switch

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <label class="sr-only" for="locale-switcher">Language Select</label>
-    <select id="locale-switcher" class="bg-transparent" v-model="locale" @change="$i18n.locale = locale">
+    <select id="locale-switcher" class="bg-transparent cursor-pointer" v-model="locale" @change="$i18n.locale = locale">
       <option value="en-US">English</option>
       <option value="es-AR">Español</option>
       <option value="pt-BR">Português</option>
