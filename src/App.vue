cf6f3e73b0b43e69b954c9b4946b5254441308d5
Pablo Miceli <d1urno@gmx.com>
2020-07-31 17:58:58 -0300

    Turn email to link

--- src/App.vue
+++ src/App.vue
@@ -165,7 +165,13 @@
               </tr>
               <tr>
                 <td class="pr-5 text-gray-900 dark:text-gray-400">E-mail:</td>
-                <td class="text-gray-900 dark:text-gray-400"><a class="hover:text-teal-400 dark:text-green-500 dark-hover:text-teal-400" href="mailto:d1urno@gmx.com">d1urno@gmx.com</a></td>
+                <td class="text-gray-900 dark:text-gray-400">
+                  <a
+                    class="text-teal-800 hover:text-teal-500 dark:text-green-500 dark-hover:text-teal-400"
+                    href="mailto:d1urno@gmx.com"
+                    >d1urno@gmx.com</a
+                  >
+                </td>
               </tr>
               <tr>
                 <td class="pr-5 text-gray-900 dark:text-gray-400">{{ $t('about.location') }}:</td>
