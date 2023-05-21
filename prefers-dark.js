83c51e4b609d07104e8dd9dda1e711b138c31d20
Pablo Miceli <d1urno@gmx.com>
2023-05-21 21:04:31 +0200

    style: set dark mode

---
+++ prefers-dark.js
@@ -0,0 +1,19 @@
+function checkDarkMode() {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+}
+
+function watchDarkMode() {
+  if (!window.matchMedia) return
+  window.matchMedia('(prefers-color-scheme: dark)').addListener(addDarkModeSelector)
+}
+
+function addDarkModeSelector() {
+  if (checkDarkMode()) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+}
+
+addDarkModeSelector()
+watchDarkMode()
