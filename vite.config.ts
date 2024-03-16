fb6a074074560af922134797231f8bc6861243f1
Pablo Miceli <d1urno@gmx.com>
2024-03-16 13:43:50 +0200

    workflow: setup unplugin-vue-i18n

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,25 @@ import vue from '@vitejs/plugin-vue'
 import Layouts from 'vite-plugin-vue-layouts'
 import Pages from 'vite-plugin-pages'
 import svgLoader from 'vite-svg-loader'
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 import UnheadVite from '@unhead/addons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), Layouts(), svgLoader(), UnheadVite()],
+  plugins: [
+    vue(),
+    // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
+    VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      fullInstall: true,
+      include: [fileURLToPath(new URL('./src/assets/locales/**', import.meta.url))]
+    }),
+    Pages(),
+    Layouts(),
+    svgLoader(),
+    UnheadVite()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
